313071d898a483ccb2fbba8621a563273a6ecc63
Artem Zekov <mrstalon@MacBook-Air-Artem.local>
2018-09-11 22:54:04 +0300

    mission completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+
+  const arrayOfNumbers = equation.match(/[0-9]+/g);
+  const arrayOfSigns = equation.match(/[+-]+/g);
+  arrayOfNumbers.splice(1, 1);
+
+  if (arrayOfSigns.length === 3) {
+    var squareDegree = Number(arrayOfSigns[0] + arrayOfNumbers[0]);
+    var firstDegree = Number(arrayOfSigns[1] + arrayOfNumbers[1]);
+    var freeСoefficient = Number(arrayOfSigns[2] + arrayOfNumbers[2]);
+  } else {
+    var squareDegree = Number(arrayOfNumbers[0]);
+    var firstDegree = Number(arrayOfSigns[0] + arrayOfNumbers[1]);
+    var freeСoefficient = Number(arrayOfSigns[1] + arrayOfNumbers[2]);
+  }
+
+  const descriminant = firstDegree * firstDegree - (4 * squareDegree * freeСoefficient);
+
+  const firstRoot = Math.round( (-firstDegree + Math.sqrt(descriminant)) / (2 * squareDegree));
+  const secondRoot = Math.round( (-firstDegree - Math.sqrt(descriminant)) / (2 * squareDegree));
+  const result = [firstRoot, secondRoot];
+
+  return result.sort((a, b) => a - b);
 }
